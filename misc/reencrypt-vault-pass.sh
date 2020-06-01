27b8422fa6fa68bea497b47272387468122a8797
Robert McQueen <rob@endlessm.com>
2020-06-01 10:29:33 +0100

    Fall back from gpg2 to gpg binary name for GNUPG

--- misc/reencrypt-vault-pass.sh
+++ misc/reencrypt-vault-pass.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-exec gpg2 --batch --decrypt $(dirname $0)/vault-password.gpg | gpg --armor --recipient 487328A9 --recipient E06F63B5 --recipient B76C70E9 --recipient C7EC6914  -e -o $(dirname $0)/vault-password.gpg
+for i in gpg2 gpg; do GPG=$i; which $i >/dev/null && break; done
+
+exec ${GPG} --batch --decrypt $(dirname $0)/vault-password.gpg | gpg --armor --recipient 487328A9 --recipient E06F63B5 --recipient B76C70E9 --recipient C7EC6914  -e -o $(dirname $0)/vault-password.gpg
